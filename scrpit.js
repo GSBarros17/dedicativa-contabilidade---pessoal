753fb03a7a22979b81edcce35883c284f9b2b69a
Guilherme Barros <gsbguilherme23@gmail.com>
2023-08-15 15:38:22 -0300

    estilização do cartão do desenvolvedor no footer

--- scrpit.js
+++ scrpit.js
@@ -2,7 +2,6 @@
 
 const buttonMenu = document.querySelector(".button-menu-ative")
 const menu = document.querySelector(".navigation-bar")
-
 buttonMenu.addEventListener('click', () => {
     menu.classList.toggle("navigation-bar-active")
 })
@@ -11,6 +10,18 @@ const changeTheme = document.querySelector(".btn-light");
 changeTheme.addEventListener("change", () => {
     document.body.classList.toggle("dark")
 })
+
+const buttonDevCard = document.querySelector(".button-dev-card")
+const modal = document.querySelector("dialog")
+const buttonCloseDevCard = document.querySelector(".button-close-dev-card")
+buttonDevCard.addEventListener("click", () => {
+    modal.showModal()
+})
+buttonCloseDevCard.addEventListener("click", () => {
+    modal.close()
+})
+
+
 
 /* capturando a data atual do sistema */
 let anoAtual = new Date().getFullYear()
